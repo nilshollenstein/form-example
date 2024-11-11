aa4c280ba6db4db326c55561c41861a911432198
nils.hollenstein <nils.hollenstein@gmail.com>
2024-11-11 10:57:48 +0100

    added form elements

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ function App() {
       <body>
         <form>
           <label for="vorname">Vorname</label><br></br>
-          <input type='text' id="vorname" name='vorname' required></input>
+          <input type='text' id="vorname" name='vorname' required></input><br></br>
+          
+          <label for='nachname'>Nachname</label><br></br>
+          <input type='text' id='nachname' name='nachname' required></input> <br></br>
+          
+          <label for='geburtsdatum'>Geburtsdatum</label><br></br>
+          <input type='date' id='geburtsdatum' name='geburtsdatum'></input>
+          <br></br>
           <input type='submit'></input>
+
         </form>
       </body>
     </div>
