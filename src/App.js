cf3a76373c942a6bdadcef53b5d4c36664b5deec
nils.hollenstein <nils.hollenstein@gmail.com>
2024-11-18 09:41:19 +0100

    changed topic

--- src/App.js
+++ src/App.js
@@ -7,20 +7,29 @@ function App() {
       
       <body>
         <form>
+          <h1>Arzttermin </h1>
           <label for="vorname">Vorname</label><br></br>
           <input type='text' id="vorname" name='vorname' required></input><br></br>
           
           <label for='nachname'>Nachname</label><br></br>
           <input type='text' id='nachname' name='nachname' required></input> <br></br>
           <label for='Gewicht'>Gewicht</label><br></br>
-          <input type='number' id='Gewicht' name='Gewicht' required></input>
+          <input type='number' id='Gewicht' name='Gewicht' required></input><br></br>
+          <label for='handynummer'>Handynummer</label><br></br>
+          <input type='tel' id='handynummer' name='handynummer' required></input><br></br>
+          <label for='hight'>Grösse</label><br></br><input type='number' id='hight' name='hight' required></input><br></br>
+
+          <label for='email'>Email</label><br></br>
+          <input type='email' id='email' name='email'></input><br></br>
           <label for='geburtsdatum'>Geburtsdatum</label><br></br>
-          <input type='date' id='geburtsdatum' name='geburtsdatum' required></input>
-<br></br>
-          <label for='todesdatum'>Todesdatum</label>
-          <br></br>
-          <input type='date' id='todesdatum' name='todesdatum' required></input><br></br>
-          <input type='submit'></input>
+          <input type='date' id='geburtsdatum' name='geburtsdatum' required></input><br></br>
+          <label for='termindatum'>Termindatum</label><br></br>
+          <input type='date' id='termindatum' name='termindatum' required></input><br></br>
+          <label for='krankenkassennummer'>Krankenkassennummer</label><br></br>
+          <input type='number' id='krankenkassennummer' name='krankenkassennummer' required></input><br></br>
+        
+          
+          <input type='submit' ></input>
 
         </form>
       </body>
